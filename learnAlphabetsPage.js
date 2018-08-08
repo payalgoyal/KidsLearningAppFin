09610aa62eb5b03fdb5562af90557ef8770147ea
payalgoyal <payalgoyal90@gmail.com>
2018-08-08 19:48:17 +0530

    added name to A For words

--- learnAlphabetsPage.js
+++ learnAlphabetsPage.js
@@ -4,16 +4,28 @@ function learnAlphabetsPage(page){
 	if(page=='1'){
 		document.getElementById('prevButton').disabled = 'true';
 		document.getElementById('letterFor').innerHTML = "<div style='float:left;width:50%'>\
-		<img src='images/alphabetsImages/A/apple.png' style='width:100%;height:60%'>\
+		<img src='images/alphabetsImages/A/apple.png' style='width:100%;height:40%'>\
 		</div>\
 		<div style='float:right;width:50%'>\
-		<img src='images/alphabetsImages/A/ant.png' style='width:100%;height:60%'>\
+		<img src='images/alphabetsImages/A/ant.png' style='width:100%;height:40%'>\
+		</div>\
+		<div style='text-align:center;width:50%;float:left'>\
+		<span class='wordFor'>APPLE</span>\
+		</div>\
+		<div style='text-align:center;width:50%;float:right'>\
+		<span class='wordFor' >ANT</span>\
 		</div>\
 		<div style='float:left;width:50%'>\
-		<img src='images/alphabetsImages/A/aeroplane.png' style='width:100%;height:60%'>\
+		<img src='images/alphabetsImages/A/aeroplane.png' style='width:100%;height:40%'>\
 		</div>\
 		<div style='float:right;width:50%'>\
-		<img src='images/alphabetsImages/A/axe.png' style='width:100%;height:60%'>\
+		<img src='images/alphabetsImages/A/axe.png' style='width:100%;height:40%'>\
+		</div>\
+		<div style='text-align:center;width:50%;float:left'>\
+		<span class='wordFor'>AEROPLANE</span>\
+		</div>\
+		<div style='text-align:center;width:50%;float:right'>\
+		<span class='wordFor'>AXE</span>\
 		</div>\
 		";
 	}
--- learnNumberPage.js
+++ learnNumberPage.js
@@ -3,202 +3,202 @@ function learnNumberPage(page){
 	pageNumber = page;
 	if(page=='1'){
 		document.getElementById('prevButton').disabled = 'true';
-		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>1</td><td class='number'>ONE</td></tr><tr> \
+		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>1</td><td class='numberSpell'>ONE</td></tr><tr> \
 			<td class='number'>2</td>\
-			<td class='number'>TWO</td>\
+			<td class='numberSpell'>TWO</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>3</td>\
-			<td class='number'>THREE</td>\
+			<td class='numberSpell'>THREE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>4</td>\
-			<td class='number'>FOUR</td>\
+			<td class='numberSpell'>FOUR</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>5</td>\
-			<td class='number'>FIVE</td>\
+			<td class='numberSpell'>FIVE</td>\
 		</tr>\
 		<tr>\
 			<td class='number' >6</td>\
-			<td class='number' >SIX</td>\
+			<td class='numberSpell' >SIX</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>7</td>\
-			<td class='number'>SEVEN</td>\
+			<td class='numberSpell'>SEVEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>8</td>\
-			<td class='number'>EIGHT</td>\
+			<td class='numberSpell'>EIGHT</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>9</td>\
-			<td class='number'>NINE</td>\
+			<td class='numberSpell'>NINE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>10</td>\
-			<td class='number'>TEN</td>\
+			<td class='numberSpell'>TEN</td>\
 		</tr>\
 		";
 	}
 	
 	if(page=='2'){
-		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>11</td><td class='number'>ELEVEN</td></tr><tr> \
+		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>11</td><td class='numberSpell'>ELEVEN</td></tr><tr> \
 			<td class='number'>12</td>\
-			<td class='number'>TWELVE</td>\
+			<td class='numberSpell'>TWELVE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>13</td>\
-			<td class='number'>THIRTEEN</td>\
+			<td class='numberSpell'>THIRTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>14</td>\
-			<td class='number'>FOURTEEN</td>\
+			<td class='numberSpell'>FOURTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>15</td>\
-			<td class='number'>FIFTEEN</td>\
+			<td class='numberSpell'>FIFTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>16</td>\
-			<td class='number'>SIXTEEN</td>\
+			<td class='numberSpell'>SIXTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>17</td>\
-			<td class='number'>SEVENTEEN</td>\
+			<td class='numberSpell'>SEVENTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>18</td>\
-			<td class='number'>EIGHTEEN</td>\
+			<td class='numberSpell'>EIGHTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>19</td>\
-			<td class='number'>NINTEEN</td>\
+			<td class='numberSpell'>NINTEEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>20</td>\
-			<td class='number'>TWENTY</td>\
+			<td class='numberSpell'>TWENTY</td>\
 		</tr>\
 		";
 	}
 	
 	if(page=='3'){
-		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>21</td><td class='number'>TWENTY ONE</td></tr><tr> \
+		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>21</td><td class='numberSpell'>TWENTY ONE</td></tr><tr> \
 			<td class='number'>22</td>\
-			<td class='number'>TWENTY TWO</td>\
+			<td class='numberSpell'>TWENTY TWO</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>23</td>\
-			<td class='number'>TWENTY THREE</td>\
+			<td class='numberSpell'>TWENTY THREE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>24</td>\
-			<td class='number'>TWENTY FOUR</td>\
+			<td class='numberSpell'>TWENTY FOUR</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>25</td>\
-			<td class='number'>TWENTY FIVE</td>\
+			<td class='numberSpell'>TWENTY FIVE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>26</td>\
-			<td class='number'>TWENTY SIX</td>\
+			<td class='numberSpell'>TWENTY SIX</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>27</td>\
-			<td class='number'>TWENTY SEVEN</td>\
+			<td class='numberSpell'>TWENTY SEVEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>28</td>\
-			<td class='number'>TWENTY EIGHT</td>\
+			<td class='numberSpell'>TWENTY EIGHT</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>29</td>\
-			<td class='number'>TWENTY NINE</td>\
+			<td class='numberSpell'>TWENTY NINE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>30</td>\
-			<td class='number'>THIRTY</td>\
+			<td class='numberSpell'>THIRTY</td>\
 		</tr>\
 		";
 	}
 	
 	if(page=='4'){
-		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>31</td><td class='number'>THIRTY ONE</td></tr><tr> \
+		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>31</td><td class='numberSpell'>THIRTY ONE</td></tr><tr> \
 			<td class='number'>32</td>\
-			<td class='number'>THIRTY TWO</td>\
+			<td class='numberSpell'>THIRTY TWO</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>33</td>\
-			<td class='number'>THIRTY THREE</td>\
+			<td class='numberSpell'>THIRTY THREE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>34</td>\
-			<td class='number'>THIRTY FOUR</td>\
+			<td class='numberSpell'>THIRTY FOUR</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>35</td>\
-			<td class='number'>THIRTY FIVE</td>\
+			<td class='numberSpell'>THIRTY FIVE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>36</td>\
-			<td class='number'>THIRTY SIX</td>\
+			<td class='numberSpell'>THIRTY SIX</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>37</td>\
-			<td class='number'>THIRTY SEVEN</td>\
+			<td class='numberSpell'>THIRTY SEVEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>38</td>\
-			<td class='number'>THIRTY EIGHT</td>\
+			<td class='numberSpell'>THIRTY EIGHT</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>39</td>\
-			<td class='number'>THIRTY NINE</td>\
+			<td class='numberSpell'>THIRTY NINE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>40</td>\
-			<td class='number'>FORTY</td>\
+			<td class='numberSpell'>FORTY</td>\
 		</tr>\
 		";
 	}
 	
 	if(page=='5'){
 		document.getElementById('nextButton').disabled = 'true';
-		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>41</td><td class='number'>FORTY ONE</td></tr><tr> \
+		document.getElementById('numberTable').innerHTML = "<tr><td class='number'>41</td><td class='numberSpell'>FORTY ONE</td></tr><tr> \
 			<td class='number'>42</td>\
-			<td class='number'>FORTY TWO</td>\
+			<td class='numberSpell'>FORTY TWO</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>43</td>\
-			<td class='number'>FORTY THREE</td>\
+			<td class='numberSpell'>FORTY THREE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>44</td>\
-			<td class='number'>FORTY FOUR</td>\
+			<td class='numberSpell'>FORTY FOUR</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>45</td>\
-			<td class='number'>FORTY FIVE</td>\
+			<td class='numberSpell'>FORTY FIVE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>46</td>\
-			<td class='number'>FORTY SIX</td>\
+			<td class='numberSpell'>FORTY SIX</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>47</td>\
-			<td class='number'>FORTY SEVEN</td>\
+			<td class='numberSpell'>FORTY SEVEN</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>48</td>\
-			<td class='number'>FORTY EIGHT</td>\
+			<td class='numberSpell'>FORTY EIGHT</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>49</td>\
-			<td class='number'>FORTY NINE</td>\
+			<td class='numberSpell'>FORTY NINE</td>\
 		</tr>\
 		<tr>\
 			<td class='number'>50</td>\
-			<td class='number'>FIFTY</td>\
+			<td class='numberSpell'>FIFTY</td>\
 		</tr>\
 		";
 	}
