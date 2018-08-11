19c16cfda6538a223dfb5bc40a16e99f3f0d0ca1
payalgoyal <payalgoyal90@gmail.com>
2018-08-11 15:39:12 +0530

    check start point

--- parkingActivity.js
+++ parkingActivity.js
@@ -55,11 +55,11 @@ function handleStart(evt) {
    for (var i = 0; i < touches.length; i++) {
     // log("touchstart:" + i + "...");
     ongoingTouches.push(copyTouch(touches[i]));
-    // var color = colorForTouch(touches[i]);
-    // ctx.beginPath();
-    // ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-    // ctx.fillStyle = color;
-    // ctx.fill();
+    var color = colorForTouch(touches[i]);
+    ctx.beginPath();
+    ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+    ctx.fillStyle = color;
+    ctx.fill();
     // log("touchstart:" + i + ".");
   }
 }
